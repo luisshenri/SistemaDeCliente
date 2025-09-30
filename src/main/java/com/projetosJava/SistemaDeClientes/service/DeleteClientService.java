2188054c72bc27489af480a0f6d52ca6f64d6cd0
luisshenri <luisshenri02@gmail.com>
2025-09-30 16:12:37 -0300

    Classe para deletar usuario por ID usando try catch e exeções

--- src/main/java/com/projetosJava/SistemaDeClientes/service/DeleteClientService.java
+++ src/main/java/com/projetosJava/SistemaDeClientes/service/DeleteClientService.java
@@ -6,8 +6,6 @@ import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Optional;
-
 public class DeleteClientService {
     private final ClienteRepository clienteRepository;
 
